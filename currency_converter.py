e343df877d1f474a238f83ae7d81cc78959e5d83
Lauris05-txt <lauris1087627@gmail.com>
2025-02-24 10:14:50 +0200

    Dropdown
    Needs fixing

--- currency_converter.py
+++ currency_converter.py
@@ -12,12 +12,15 @@ class Api():
         
     def get_currency_data(self, currency):
         result = self.admin.latest(base_currency=currency)
+        print(currency)
         return result
 
     def convert_currency(self, input_currency, output_currency):
         response = self.get_currency_data(input_currency)
+        # try:
         return response["data"][output_currency]
-
+        # except:
+        #     KeyError
 converter = Api()
 # a.get_currency_data()
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -25,16 +28,24 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route("/", methods = ["GET", "POST"])
 def index():
     output_sum = ""
+    converter_error = ""
     if request.method == "POST":
-        input_currency = request.form.get("input_currency")
-        output_currency = request.form.get("output_currency")
-        input_sum = request.form.get("input_sum")
-        
-        output_conversion = converter.convert_currency(input_currency, output_currency)
-        output_sum = float(output_conversion) * float(input_sum)
+        try:
+            input_currency = request.form.get("input_currency")
+            output_currency = request.form.get("output_currency")
+            input_sum = request.form.get("input_sum")
+            print(input_currency, output_currency, input_sum)
+            output_conversion = converter.convert_currency(input_currency, output_currency)
+            output_sum = float(output_conversion) * float(input_sum)
+        except KeyError as e:
+            converter_error = f"KeyError: {e}"
+        except ValueError as e:
+            converter_error = f"ValueError: {e}"
+        except Exception as e:
+            converter_error = f"Error: {e}"
 
     print(output_sum)
-    return render_template("index.html", output_sum=output_sum)
+    return render_template("index.html", output_sum=output_sum, converter_error=converter_error)
     
 if __name__ == '__main__':
     app.run(debug=True)
